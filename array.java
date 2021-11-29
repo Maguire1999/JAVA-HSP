340c835411be9b77c14e099a639ab5285c24b908
Maguire1999 <610716365@qq.com>
2021-11-29 22:40:14 +0800

    值传递与引用传递

--- array.java
+++ array.java
@@ -3,6 +3,24 @@
 public class array {
     public static void main(String[] args) {
     
-        System.out.println("Hello World\n");    
+        // System.out.println("Hello World\n");
+        // 1\基本数据类型赋值 数拷贝
+        int n1 = 10;
+        int n2 = n1;
+        n2  = 80;
+        System.out.println("n1 = " + n1);
+        System.out.println("n2 = " + n2);
+        // 2\数组默认是引用传递，赋值的是地址，赋值方式位引用传递
+        int[] arr1 = {1,2,3};
+        int[] arr2 = arr1;
+
+        arr2[0] = 100;
+        System.out.println("******arr1的元素*******");
+        for(int i = 0 ; i<arr1.length ; i++ ){
+            System.out.println(arr1[i]);
+        }
+        System.out.println(" arr1[0] : " + arr1[0]);
+        System.out.println(" arr2[0] : " + arr2[0]);
+
     }
 }
